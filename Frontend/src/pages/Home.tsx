2ef6ed62db931f94e9519c7257c89ed1ef5fea26
Kasper Siewierkiewicz <117650279+Kasper-Sie@users.noreply.github.com>
2025-04-30 15:11:05 +0200

    newest

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -4,35 +4,12 @@ import Map from "../components/map";
 
 function Home() {
     const [visiblePlaces, setVisiblePlaces] = useState<any[]>([]);
-    const [selectedPlacesList, setSelectedPlacesList] = useState<any[]>([]);
-
-    const handleAddPlace = (place: any) => {
-        const exists = selectedPlacesList.some(p => p.properties?.id === place.id);
-        if (!exists) {
-            const newPlaceFeature = {
-                type: "Feature",
-                geometry: {
-                    type: "Point",
-                    coordinates: [place.longitude, place.latitude],
-                },
-                properties: {
-                    ...place,
-                },
-            };
-            setSelectedPlacesList(prev => [...prev, newPlaceFeature]);
-        }
-    };
 
     return (
         <div className="bg-background-beige1 h-screen text-text-dark flex-row">
-            <Head handleAddPlace={handleAddPlace} />
+            <Head />
             <div className="flex h-[calc(100%-60px)]">
-                <Map
-                    setVisiblePlaces={setVisiblePlaces}
-                    visiblePlaces={visiblePlaces}
-                    selectedPlacesList={selectedPlacesList}
-                    setSelectedPlacesList={setSelectedPlacesList}
-                />
+                <Map setVisiblePlaces={setVisiblePlaces} visiblePlaces={visiblePlaces} />
             </div>
         </div>
     );
