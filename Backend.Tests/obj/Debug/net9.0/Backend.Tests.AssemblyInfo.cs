fdf40ab5e59122d2f5370270038bf593e9304a22
Kasper Siewierkiewicz <117650279+Kasper-Sie@users.noreply.github.com>
2025-05-21 13:52:11 +0200

    db shit

--- Backend.Tests/obj/Debug/net9.0/Backend.Tests.AssemblyInfo.cs
+++ Backend.Tests/obj/Debug/net9.0/Backend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7518701b8f9462c677c4cdbca3a94715641d387")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba00423842e089f3752231ac679de6945e52a059")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
