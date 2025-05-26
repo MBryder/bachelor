099ae3e29fa49cd5277017f2183ef96cbb18cccd
Kasper Siewierkiewicz <117650279+Kasper-Sie@users.noreply.github.com>
2025-05-26 10:18:19 +0200

    last update

--- Backend.Tests/obj/Debug/net9.0/Backend.Tests.AssemblyInfo.cs
+++ Backend.Tests/obj/Debug/net9.0/Backend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c6ea6b86050e2b3d535296269f2bdcf9b502d5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec0c567a2e3862f02e250cc00b91ec3747f6a939")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
